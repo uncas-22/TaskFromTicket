eb229211f78b9af095dc33b3a8c0262ef5102502
Alex <seredin-alex@mail.ru>
2022-11-13 22:08:26 +0300

    Начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-     System.out.println("Hello world!");
+     String str = "abcdef";
+     String[] dividedString = dividedString(str);
+        System.out.println(dividedString[0] + "  " + dividedString[1]);
+    }
+    public static String[] dividedString(String str) {
+        String[] result = new String[2];
+        if (str.length() % 2 == 0) {
+            result[0] = str.substring(0, str.length() / 2);
+            result[1] = str.substring(str.length() / 2);
+        } else {
+            result[0] = str.substring(0, str.length() / 2);
+            result[1] = str.substring(str.length() / 2);
+        }
+        return result;
     }
 }
